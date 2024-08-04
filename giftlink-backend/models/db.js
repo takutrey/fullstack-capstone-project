834ee3c6d5830baa6556ba7bd22928532de114a1
Takudzwa Ganda <takuganda@mail.com>
2024-08-04 18:17:51 -0400

    initial commit

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,17 @@
 // db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
-
 let dbInstance = null;
 const dbName = "giftdb";
-
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
-
-    const client = new MongoClient(url);      
-
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    const client = new MongoClient(url);
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
-
 module.exports = connectToDatabase;
