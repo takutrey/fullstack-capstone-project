48e0dcd82a348dc4878446aa586152e63f169055
Takudzwa Ganda <takuganda@mail.com>
2024-08-05 04:59:19 -0400

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />}
