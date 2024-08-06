dd260a87a0162b04a60445be17b49f20ce77df20
Takudzwa Ganda <takuganda@mail.com>
2024-08-05 20:55:00 -0400

    backend profile compnent

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,7 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import Profile from './components/Profile/Profile';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />}
