5e8aee06e74b8146587e27ee8af740c250a53864
takutrey <92523638+takutrey@users.noreply.github.com>
2024-08-06 03:26:36 +0200

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
+  
   return (
         <>
         <Navbar/>
